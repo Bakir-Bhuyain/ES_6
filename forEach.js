cf01c5b277ae8f7bd8a62634fbb29b9f234e3d2f
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-02-04 00:37:56 +0600

    explorig forEach in ES6

---
+++ forEach.js
@@ -0,0 +1,4 @@
+//forEach basically worked in array 
+// amr jodi out put er dorkar nai tahole ami forEach use korbo (kono ekta kaj korbo kintu amr kono return er dorkar nai etai basically forEach er kaj.)
+//jodi amr dorkar hoy output tokhn ami ma use korbo,
+const numbers = [4, 5, 6, 8, 2, 3];

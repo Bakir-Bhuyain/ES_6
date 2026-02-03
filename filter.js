13d7f308a4f89236a8d9939118329cc497c559ca
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-02-04 00:58:06 +0600

    explored flter in the modern js

---
+++ filter.js
@@ -0,0 +1,11 @@
+//filter hocche ami ekta sorto dibo and filter shei shorter basis e hocche amk jinish gula return korbe ;
+//cheke cheke dibe ;
+
+const numbers = [4, 5, 6, 10, 12, 13, 14,  8, 2, 3, 18, 19];
+
+const greaterThen = numbers.filter(x => x > 10);
+console.log(greaterThen);
+
+const names = ['bakir', 'rakib', 'abir', 'tusher', 'fahim'];
+const length = names.filter(lenPositon => lenPositon.length <= 4 );
+console.log(length);
--- forEach.js
+++ forEach.js
@@ -1,4 +1,12 @@
 //forEach basically worked in array 
 // amr jodi out put er dorkar nai tahole ami forEach use korbo (kono ekta kaj korbo kintu amr kono return er dorkar nai etai basically forEach er kaj.)
 //jodi amr dorkar hoy output tokhn ami ma use korbo,
+
+
 const numbers = [4, 5, 6, 8, 2, 3];
+
+numbers.forEach(element => {
+      const doubled = element * 2;
+      console.log(doubled)
+      // return doubled;
+});
